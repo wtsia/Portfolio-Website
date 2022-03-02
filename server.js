c5db27ad1809ff6accb47f3aed6e220c7333f037
Winston T <54168818+wtsia@users.noreply.github.com>
2022-03-01 23:54:15 -0800

    migrating code from form testing repo

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const nodemailer = require('nodemailer')
+
+const PORT = process.env.PORT || 5000
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get ('/', (req, res) => {
+    res.send('hello!')
+});
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: provess.env.EMAIL_PASS
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: '',
+        subject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.content
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent')
+            res.send('success')
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+});
